0ef2d90c43f31d4ab1a963c2b4f28632ef6728a8
edwardNumpaque <edward.numpaque@aliadoti.com>
2025-02-19 18:00:03 -0500

    Se crean componentes QuienesSomos e Historia

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { QuienesSomosComponent } from '../components/quienes-somos/quienes-somos.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+   
+];
--- src/components/content/content.component.ts
+++ src/components/content/content.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { QuienesSomosComponent } from '../quienes-somos/quienes-somos.component';
 
 @Component({
   selector: 'app-content',
-  imports: [],
+  imports: [QuienesSomosComponent],
   templateUrl: './content.component.html',
   styleUrl: './content.component.css'
 })
